a7ad9eecd506ebf975d742da23c0eab0a1f3bd18
hmagdaleno <henrique.magdaleno@outlook.com>
2022-06-27 00:23:16 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication6/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                cliente.DataCad = DateTime.Now;
+                Banco(cliente);
+                return View("Resultado", cliente);
+            }
+            return View(cliente);
+        }
+        public void Banco(Cliente cliente)
+        {
+            SqlConnection minhaConexao = new SqlConnection(@"data source=DESKTOP-UVUVP4T\SQLEXPRESS;Integrated Security=SSPI;Initial Catalog=WebApplication5");
+            minhaConexao.Open();
+            string x = string.Format ("INSERT INTO CLIENTES (Nome, CPF, Nacionalidade, Naturalidade, DataCad, Ativo) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}')",cliente.Nome,cliente.CPF,cliente.Nacionalidade,cliente.Naturalidade,cliente.DataCad,cliente.Ativo);
+            SqlCommand cmd = new SqlCommand(x,minhaConexao);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
---
+++ WebApplication6/Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication6.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Campo obrigatório.")]
+        [StringLength(70, ErrorMessage ="Digite até 70 caracteres.")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage ="Campo obrigatório.")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage ="Digite exatamente 11 caracteres.")]
+        public string CPF { get; set; }
+        public string? Nacionalidade { get; set; }
+        public string? Naturalidade { get; set; }
+        public DateTime DataCad { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
